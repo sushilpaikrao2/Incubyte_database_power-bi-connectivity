3ef278fe5875671bb72a7777e9cba79bb17de610
Sushil Paikrao <86233992+sushilpaikrao2@users.noreply.github.com>
2021-10-18 12:51:23 +0530

    Add files via upload

---
+++ Incubyte_query.sql
@@ -0,0 +1,405 @@
+CREATE TABLE patient1 (
+  Customer_Id int(18) NOT NULL,
+  Customer_Name varchar(255) NOT NULL,
+  Open_date date NOT NULL,
+  Last_Consulted_Date date NOT NULL,
+  Vaccination_Id char(5) NOT NULL,
+  Dr_Name varchar(45) DEFAULT NULL,
+  State char(5) DEFAULT NULL,
+  Country char(5) NOT NULL,
+  DOB date DEFAULT NULL,
+  Is_Active char(3) NOT NULL,
+  PRIMARY KEY (` Customer_Id`)
+) 
+select*from patient1;
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(123,
+'Akshay',
+20210901,
+20220902,
+'MVD',
+'Paul',
+'MH',
+'IND',
+'1994-07-04',
+'A' );
+
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_DateConsult_dt,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(345,
+'Sushil',
+20210805,
+20220805,
+'CSD',
+'Alex',
+'SA',
+'USA',
+'1995-08-04',
+'A' );
+
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(456,
+'Shital',
+20210905,
+20221005,
+'MVD',
+'John',
+'UP',
+'IND',
+'1994-11-04',
+'A' );
+
+
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(567,
+'Rakhi',
+20211011,
+20221011,
+'MVD',
+'Mathew',
+'BOS',
+'NYC',
+'1996-07-04',
+'A' );
+
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(678,
+'Chetna',
+20211011,
+20221011,
+'CSD',
+'Shital',
+'MH',
+'IND',
+'1996-07-04',
+'A' );
+
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(789,
+'kedar',
+20210507,
+20220507,
+'MVD',
+'Matt',
+'TN',
+'IND',
+'1993-02-01',
+'A' );
+
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(987,
+'kumar',
+20210409,
+20220409,
+'CSD',
+'Piter',
+'CO',
+'UK',
+'1993-10-03',
+'A' );
+
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(876,
+'simran',
+20210108,
+20220108,
+'CSD',
+'Mathew',
+'AL',
+'UK',
+'1998-08-03',
+'A' );
+
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(765,
+'kajol',
+20210209,
+20220209,
+'MVD',
+'Paul',
+'GJ',
+'IND',
+'1994-04-08',
+'A' );
+
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(111,
+'samiksha',
+20210610,
+20220610,
+'MVD',
+'piter',
+'VG',
+'USA',
+'1994-03-05',
+'A' );
+
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(653,
+'Arjun',
+20210209,
+20220710,
+'MVD',
+'John',
+'SG',
+'UK',
+'1994-06-07',
+'A' );
+
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(541,
+'Amrish',
+20210507,
+20220507,
+'MVD',
+'Pooja',
+'CA',
+'USA',
+'1998-03-01',
+'A' );
+
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(541,
+'Amrish',
+20210507,
+20220507,
+'MVD',
+'Pooja',
+'CA',
+'USA',
+'1998-03-01',
+'A' );
+
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(432,
+'pooja',
+20210806,
+20220806,
+'CSD',
+'Pooja',
+'CO',
+'UK',
+'1995-07-06',
+'A' );
+
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(489,
+'prem',
+20210502,
+20220502,
+'CSD',
+'Pooja',
+'CO',
+'UK',
+'1995-07-06',
+'A' );
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(852,
+'john',
+20210507,
+20220507,
+'CSD',
+'Matt',
+'CO',
+'USA',
+'1992-07-07',
+'A' );
+INSERT INTO patient1
+(Customer_Id,
+Customer_Name,
+Open_date,
+Last_Consulted_Date,
+Vaccination_Id,
+Dr_Name,
+State,
+Country,
+DOB,
+Is_Active)
+VALUES
+(753,
+'asho',
+20210508,
+20220508,
+'MVD',
+'pitet',
+'KL',
+'IND',
+'1992-07-07',
+'A' );
+
+
